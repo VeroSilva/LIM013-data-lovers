181936773b8b95794b8f70e9d8377bc06bc6a5a8
Veronica Silva <veronicasilvabarreto@gmail.com>
2020-08-23 18:49:33 -0500

    Cambio de la imagen de cabecera mas estilo, función de filtrado por tipo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const typeButton = document.getElementsByClassName("type");
+
+for (var i = 0; i < typeButton.length; i++) {
+    typeButton[i].addEventListener("click", function(event) {
+        let dataType = event.target.getAttribute("data-type");
+        typeShow(dataType);
+    })
+}
+
+function typeShow(e) {
+    data.pokemon.forEach(element => {
+        if (element.type.includes(e)) {
+            console.log(element.name)
+        }
+    });
+}
