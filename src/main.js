d9045ab8e37b0e58581a3d25edd0b38a7d82b282
Illari Alvarez-Gil <friedapples824@gmail.com>
2020-08-26 19:09:28 -0500

    Se da funcionalidad a botones de paginación

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import dataFile from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-//Agregar clase a cada pokemon según su tipo
+//Agregar clase a cada pokemon según su tipo- Paginación
 let eachPokemon = data.pokemon;
-for (let k = 0; k < eachPokemon.length; k++) {
-    let container = document.getElementById("container");
+let page = 0;
+let lista= [];
+let container = document.getElementById("container");
+for (let k = 0; k < page +6 ; k++) {
     let cardNew = document.createElement("div");
     let image1 = document.createElement("img");
     let text1 = document.createElement("h2");
@@ -14,13 +16,15 @@ for (let k = 0; k < eachPokemon.length; k++) {
     text1.textContent = eachPokemon[k].name;
     cardNew.appendChild(image1);
     cardNew.appendChild(text1);
+    lista.push(cardNew);
     container.appendChild(cardNew);
     let eachTypePoke = eachPokemon[k].type;
-    for (let l = 0; l < eachTypePoke.length; l++) {
+    for (let l = 0; l < eachPokemon.length; l++) {
         cardNew.classList.add(eachTypePoke[l]);
     }
+    
 }
-
+console.log(lista.length)
 //Función de botones para filtrar por tipo
 const typeButton = document.getElementsByClassName("type");
 for (let j = 0; j < typeButton.length; j++) {
@@ -43,28 +47,45 @@ buttonMenuTypes.addEventListener("click", function() {
     document.getElementById("menu-sidebar").classList.add("display");
     document.getElementById("menu-sidebar").classList.remove("hide");
 })
-//Paginacion
-const pokeCards= document.getElementsByClassName("card");
-console.log(pokeCards);
-const pokeGallery =document.getElementById("container");
-let currentPage = 1;
-let rows = 2;
-function displayList(items, wrapper, rowsPerPage, page){
-    wrapper.innerHTML = "";
-	page--;
+//Paginacion 
+const first = document.querySelector(".first");
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
+const last = document.querySelector(".last");
+//Funionalidad botón next
+next.addEventListener ("click", function(){
+    page==lista.length - 6 ? (page == 0):(page += 10);
+    container.innerHTML = "";
+    for(let o = page; o < page + 6; o++){
+        let cardNew = document.createElement("div");
+        let image1 = document.createElement("img");
+        let text1 = document.createElement("h2");
+        cardNew.className = "card";
+        image1.className = "imageContainer";
+        image1.setAttribute("src", eachPokemon[o].img);
+        text1.textContent = eachPokemon[o].name;
+        cardNew.appendChild(image1);
+        cardNew.appendChild(text1);
+        lista.push(cardNew);
+        container.appendChild(cardNew);
+     }
+})
+//Funcionalidad botón previous
+previous.addEventListener("click", function(){
+    page== 0 ? (page = lista.length-10):(page-=10);
+    container.innerHTML = "";
+    for(let o = page; o < page + 6; o++){
+        let cardNew = document.createElement("div");
+        let image1 = document.createElement("img");
+        let text1 = document.createElement("h2");
+        cardNew.className = "card";
+        image1.className = "imageContainer";
+        image1.setAttribute("src", eachPokemon[o].img);
+        text1.textContent = eachPokemon[o].name;
+        cardNew.appendChild(image1);
+        cardNew.appendChild(text1);
+        lista.push(cardNew);
+        container.appendChild(cardNew);
+     }
 
-	let start = rowsPerPage * page;
-	let end = start + rowsPerPage;
-	let paginatedItems = items.slice(start, end);
-
-	for (let i = 0; i < paginatedItems.length; i++) {
-		let item = paginatedItems[i];
-
-		let item_element = document.createElement('div');
-		item_element.classList.add('item');
-		item_element.innerText = item;
-		
-		wrapper.appendChild(item_element);
-	}
-}
-
+})
